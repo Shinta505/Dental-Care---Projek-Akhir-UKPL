2cefca62cb6dab88fa74b15611e1c5f53845f8ed
Shinta Nursobah Chairani <shinta.nur505@gmail.com>
2026-09-03 11:14:27 +0700

    Enhance database connection configuration
    Added port configuration and SSL options for database connection.

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,20 +1,22 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv"; //untuk menyembunyikan data berharga
+import dotenv from "dotenv";
 
-dotenv.config();//ngekonfigurasi data dari file.env
+dotenv.config();
 
-
-//Bikin variabel yg nerima data yg dirahasiakan
 const DB_NAME = process.env.DB_NAME;
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
-
-
-// Nyambungin db ke BE
 const db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: "mysql",
+  dialectOptions: {
+    ssl: {
+      require: true, // 2. Wajib aktifkan SSL untuk Aiven
+      rejectUnauthorized: false // 3. Abaikan verifikasi sertifikat lokal
+    }
+  }
 });
 
 export default db;
